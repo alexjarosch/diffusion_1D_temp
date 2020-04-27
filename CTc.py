208c2b7aca928a3d0092c0810fbdf51370d09fdb
Alexander Jarosch <research@alexj.at>
2020-04-27 09:25:11 +0200

    cave layer removed, not necassery for science task

--- CTc.py
+++ CTc.py
@@ -9,7 +9,7 @@ __author__ = "Alexander H. Jarosch (research@alexj.at)"
 __date__ = "2020-01-29 --"
 __copyright__ = "Copyright (C) 2020 ThetaFrame Solutions"
 __license__ = "GNU GPL Version 3"
-__version__ = "2.0"
+__version__ = "3.1"
 
 """
 This script is free software: you can redistribute it and/or modify
@@ -31,90 +31,83 @@ sec_in_year = 365.25 * 24 * 3600
 
 window = tk.Tk()
 window.title("Cave Heat Conduction Model with Snow")
-window.geometry('600x500')
+window.geometry('700x500')
 
 # input for depth
-lbl_dc = tk.Label(window, text="Depth to Cave [m]")
+lbl_dc = tk.Label(window, text="Depth to Simulate [m]")
 lbl_dc.grid(column=0, row=0, sticky="W")
 txt_dc = tk.Entry(window, width=10)
-txt_dc.insert(tk.END, '60.0')
+txt_dc.insert(tk.END, '100.0')
 txt_dc.grid(column=1, row=0)
 
-# input for cave
-lbl_hc = tk.Label(window, text="Height of Cave [m]")
-lbl_hc.grid(column=0, row=1, sticky="W")
-txt_hc = tk.Entry(window, width=10)
-txt_hc.insert(tk.END, '6.0')
-txt_hc.grid(column=1, row=1)
-
 lbl_zres = tk.Label(window, text="Vertical Resolution N")
-lbl_zres.grid(column=0, row=2, sticky="W")
+lbl_zres.grid(column=0, row=1, sticky="W")
 txt_zres = tk.Entry(window, width=10)
-txt_zres.insert(tk.END, '21')
-txt_zres.grid(column=1, row=2)
+txt_zres.insert(tk.END, '51')
+txt_zres.grid(column=1, row=1)
 
 # input for jan tmp
 lbl_jaT = tk.Label(window, text="January Mean TMP [C]")
-lbl_jaT.grid(column=0, row=3, sticky="W")
+lbl_jaT.grid(column=0, row=2, sticky="W")
 txt_jaT = tk.Entry(window, width=10)
-txt_jaT.insert(tk.END, '-4.0')
-txt_jaT.grid(column=1, row=3)
+txt_jaT.insert(tk.END, '-12.0')
+txt_jaT.grid(column=1, row=2)
 
 # input for jul tmp
 lbl_juT = tk.Label(window, text="July Mean TMP [C]")
-lbl_juT.grid(column=0, row=4, sticky="W")
+lbl_juT.grid(column=0, row=3, sticky="W")
 txt_juT = tk.Entry(window, width=10)
-txt_juT.insert(tk.END, '18.0')
-txt_juT.grid(column=1, row=4)
+txt_juT.insert(tk.END, '8.0')
+txt_juT.grid(column=1, row=3)
 
 # input for snow delta_tmp
 lbl_sdT = tk.Label(window, text="Snow delta TMP [C]")
-lbl_sdT.grid(column=0, row=5, sticky="W")
+lbl_sdT.grid(column=0, row=4, sticky="W")
 txt_sdT = tk.Entry(window, width=10)
-txt_sdT.insert(tk.END, '7.0')
-txt_sdT.grid(column=1, row=5)
+txt_sdT.insert(tk.END, '0.0')
+txt_sdT.grid(column=1, row=4)
 
 # input for soil temp
 lbl_iTg = tk.Label(window, text="Initial Ground TMP [C]")
-lbl_iTg.grid(column=0, row=6, sticky="W")
+lbl_iTg.grid(column=0, row=5, sticky="W")
 txt_iTg = tk.Entry(window, width=10)
-txt_iTg.insert(tk.END, '0')
-txt_iTg.grid(column=1, row=6)
+txt_iTg.insert(tk.END, '-2.0')
+txt_iTg.grid(column=1, row=5)
 
 # input for cave temp
-lbl_iTc = tk.Label(window, text="Initial Cave TMP [C]")
-lbl_iTc.grid(column=0, row=7, sticky="W")
-txt_iTc = tk.Entry(window, width=10)
-txt_iTc.insert(tk.END, '-2')
-txt_iTc.grid(column=1, row=7)
+lbl_gHf = tk.Label(window, text="Ground Heat Flux [W/m^2]")
+lbl_gHf.grid(column=0, row=6, sticky="W")
+txt_gHf = tk.Entry(window, width=10)
+txt_gHf.insert(tk.END, '0.05')
+txt_gHf.grid(column=1, row=6)
 
 # input for thermal diffusivity
 lbl_alphaG = tk.Label(window, text="Thermal Diffusivity Ground [m^2 / s]")
-lbl_alphaG.grid(column=0, row=8, sticky="W")
+lbl_alphaG.grid(column=0, row=7, sticky="W")
 txt_alphaG = tk.Entry(window, width=10)
-txt_alphaG.insert(tk.END, ' 3.0e-6')
-txt_alphaG.grid(column=1, row=8)
+txt_alphaG.insert(tk.END, '1.0e-6')
+txt_alphaG.grid(column=1, row=7)
 
 # input for thermal diffusivity
-lbl_alphaA = tk.Label(window, text="Thermal Diffusivity Air [m^2 / s]")
-lbl_alphaA.grid(column=0, row=9, sticky="W")
-txt_alphaA = tk.Entry(window, width=10)
-txt_alphaA.insert(tk.END, '1.96e-5')
-txt_alphaA.grid(column=1, row=9)
+lbl_kG = tk.Label(window, text="Thermal Conductivity Ground [W / (m K)]")
+lbl_kG.grid(column=0, row=8, sticky="W")
+txt_kG = tk.Entry(window, width=10)
+txt_kG.insert(tk.END, '2.0')
+txt_kG.grid(column=1, row=8)
 
 # input for model runtime
 lbl_rtime = tk.Label(window, text="Model Runtime [years]")
-lbl_rtime.grid(column=0, row=10, sticky="W")
+lbl_rtime.grid(column=0, row=9, sticky="W")
 txt_rtime = tk.Entry(window, width=10)
-txt_rtime.insert(tk.END, '10')
-txt_rtime.grid(column=1, row=10)
+txt_rtime.insert(tk.END, '400')
+txt_rtime.grid(column=1, row=9)
 
 # input for plot interval
 lbl_intP = tk.Label(window, text="Interval to Plot [years]")
-lbl_intP.grid(column=0, row=11, sticky="W")
+lbl_intP.grid(column=0, row=10, sticky="W")
 txt_intP = tk.Entry(window, width=10)
-txt_intP.insert(tk.END, '1')
-txt_intP.grid(column=1, row=11)
+txt_intP.insert(tk.END, '50')
+txt_intP.grid(column=1, row=10)
 
 
 # forcing with annual cycle
@@ -146,14 +139,9 @@ def updateS():
 
     # do vert res update
     [zga, dz_is] = np.linspace(0, float(txt_dc.get()), int(txt_zres.get()), retstep=True)
-    z_cave_is = float(txt_dc.get()) + np.arange(dz_is, float(txt_hc.get())+dz_is, dz_is)
-    Nz_cave = len(z_cave_is)
     dz_txt = "%0.4f m" % dz_is
-    Nz_cave_txt = "%d" % Nz_cave
     lbl_dz2.configure(text=dz_txt)
     lbl_dz2.update()
-    lbl_caveP2.configure(text=Nz_cave_txt)
-    lbl_caveP2.update()
 
 
 def updateSP():
@@ -165,14 +153,9 @@ def updateSP():
 
     # do vert res update
     [zga, dz_is] = np.linspace(0, float(txt_dc.get()), int(txt_zres.get()), retstep=True)
-    z_cave_is = float(txt_dc.get()) + np.arange(dz_is, float(txt_hc.get())+dz_is, dz_is)
-    Nz_cave = len(z_cave_is)
     dz_txt = "%0.4f m" % dz_is
-    Nz_cave_txt = "%d" % Nz_cave
     lbl_dz2.configure(text=dz_txt)
     lbl_dz2.update()
-    lbl_caveP2.configure(text=Nz_cave_txt)
-    lbl_caveP2.update()
 
     plt.figure()
     plt.title("Surface TMP forcing")
@@ -200,16 +183,16 @@ def clicked():
 
     # thermal diffusivities in SI units
     alpha_ground = float(txt_alphaG.get())      # m^2 / s
-    alpha_air = float(txt_alphaA.get())         # m^2 / s
+    k_ground = float(txt_kG.get())         # m^2 / s
 
     # geometry
     depth_to_cave = float(txt_dc.get())    # m
-    height_of_cave = float(txt_hc.get())   # m
     Nz_ground = int(txt_zres.get())        # m
 
     # initial temperatures
     t_init_ground = float(txt_iTg.get())      # deg C
-    t_init_caveair = float(txt_iTc.get())     # deg C
+    # ground heat Flux
+    g_heat_flux = float(txt_gHf.get())        # W/m2
 
     # time domain
     t_save = int(txt_intP.get())       # years
@@ -220,25 +203,25 @@ def clicked():
     """ Model Code """
 
     # space domain
-    [z_ground, dz] = np.linspace(0, depth_to_cave, Nz_ground, retstep=True)
-    z_cave = depth_to_cave + np.arange(dz, height_of_cave+dz, dz)
-    z = np.hstack([z_ground, z_cave])
+    [z1, dz1] = np.linspace(0, depth_to_cave, Nz_ground, retstep=True)
+    # add a point to do the bottom BC which we don't plot
+    [z, dz] = np.linspace(0, depth_to_cave+2*dz1, Nz_ground+2, retstep=True)
     Nz = len(z)
     z_total = z[-1]
 
     # diffusivity
-    # find the soil index
-    s_idx = np.where(z <= depth_to_cave)
-    c_idx = np.where(z > depth_to_cave)
-    D = np.zeros(len(z))
-    D[s_idx] = alpha_ground
-    D[c_idx] = alpha_air
+    D = alpha_ground
+
+    # calculate the TMP gradient for the lower boundary
+    deltaT_BC = (g_heat_flux * dz)/k_ground
+    deltaT_grad = g_heat_flux/k_ground
 
     T_data = np.zeros((Nt+1, Nz))
 
     # set initial TMP conditions
-    T_data[:, s_idx] = t_init_ground
-    T_data[:, c_idx] = t_init_caveair
+    T_data[0, :] = t_init_ground + deltaT_grad*z
+    # lower boundary condition
+    # T_data[0, -2] = t_init_ground + deltaT_BC
 
     # define FDM index array for space
     # make i index array
@@ -267,9 +250,13 @@ def clicked():
 
             space_diff = u[index_i_plus] - 2*u[index_i] + u[index_i_minus]
             # boundary conditions and time step
-            u[index_i] = u[index_i] + D[index_i]*(dt_use/dz**2)*space_diff
+            u[index_i] = u[index_i] + D*(dt_use/dz**2)*space_diff
+            # upper BC
             u[0] = T_surf(t) + T_snow(t)
+            # lower BC
+            # u[Nz-1] = u[Nz-2]
             u[Nz-1] = u[Nz-2]
+            u[Nz-2] = u[Nz-1] + deltaT_BC
 
             time_info = "%0.4f years" % (t/sec_in_year)
             lbl_time2.configure(text=time_info)
@@ -282,9 +269,9 @@ def clicked():
     plt.figure()
     plt.title("TMP with depth")
     for pidx in range(len(t_solve)):
-        plt.plot(T_data[pidx, :], z, marker='.', label=("Year %d" % t_solve[pidx]))
+        plt.plot(T_data[pidx, :-2], z[:-2], marker='.', label=("Year %d" % t_solve[pidx]))
 
-    plt.plot(plt.xlim(), [depth_to_cave, depth_to_cave], '--k', label="cave roof")
+    plt.plot([0, 0], [0, z_total], '--k')
     plt.ylim(z_total, 0)
     plt.legend()
     plt.xlabel("TMP in C")
@@ -294,42 +281,37 @@ def clicked():
 
 
 lbl_info = tk.Label(window, text="Model Setup Info:")
-lbl_info.grid(column=1, row=12)
+lbl_info.grid(column=1, row=11)
 
 lbl_dz = tk.Label(window, text="Vertical Resolution dz:")
-lbl_dz.grid(column=0, row=13, sticky="W")
-lbl_dz2 = tk.Label(window, text="%0.4f m" % (3))
-lbl_dz2.grid(column=1, row=13)
+lbl_dz.grid(column=0, row=12, sticky="W")
+lbl_dz2 = tk.Label(window, text="%0.4f m" % (2))
+lbl_dz2.grid(column=1, row=12)
 
 amean_T = np.mean([float(txt_jaT.get()), float(txt_juT.get())])
 lbl_meanT = tk.Label(window, text="Annual Mean Air TMP:")
-lbl_meanT.grid(column=0, row=14, sticky="W")
+lbl_meanT.grid(column=0, row=13, sticky="W")
 lbl_meanT2 = tk.Label(window, text="%0.4f C" % amean_T)
-lbl_meanT2.grid(column=1, row=14)
-
-lbl_caveP = tk.Label(window, text="Points in Cave:")
-lbl_caveP.grid(column=0, row=15, sticky="W")
-lbl_caveP2 = tk.Label(window, text="%d" % 2)
-lbl_caveP2.grid(column=1, row=15)
+lbl_meanT2.grid(column=1, row=13)
 
 btn = tk.Button(window, text="Update Setup / Plot Input", command=updateSP)
-btn.grid(column=1, row=16)
+btn.grid(column=1, row=14)
 
 btn = tk.Button(window, text="Update Setup and Run Model", command=clicked)
-btn.grid(column=1, row=18)
+btn.grid(column=1, row=16)
 
 lbl_time = tk.Label(window, text="Model Time:")
-lbl_time.grid(column=0, row=17, sticky="W")
+lbl_time.grid(column=0, row=15, sticky="W")
 lbl_time2 = tk.Label(window, text="%0.4f years" % (0))
-lbl_time2.grid(column=1, row=17)
+lbl_time2.grid(column=1, row=15)
 
 lbl_space = tk.Label(window, text="")
-lbl_space.grid(column=0, row=19)
+lbl_space.grid(column=0, row=17)
 
 lbl_space = tk.Label(window, text="")
-lbl_space.grid(column=0, row=20)
+lbl_space.grid(column=0, row=18)
 
-lbl_CR = tk.Label(window, text="Ver. 2.0 - Copyright (C) 2020 ThetaFrame Solutions, GPLv3")
-lbl_CR.grid(column=1, row=21)
+lbl_CR = tk.Label(window, text="Ver. 3.1 - Copyright (C) 2020 ThetaFrame Solutions, GPLv3")
+lbl_CR.grid(column=1, row=19)
 
 window.mainloop()
